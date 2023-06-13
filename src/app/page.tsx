0859b9fede2fce88feeb3d4511ca1f55f0a7280e
Zak Mayfield <zakmayfield@gmail.com>
2023-06-13 16:35:53 -0400

    styling for a bit more visualization of the code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
-import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
       <div>Next with Supabase</div>
+
+      <div className='max-w-4xl mx-auto my-6 p-6 rounded-md bg-slate-200'>
+        <h2>rendering and caching data</h2>
+        <ul className='px-6'>
+          <li>
+            <Link href='/static' className='text-blue-800'>
+              /static
+            </Link>
+          </li>
+          <li>
+            <Link href='/dynamic' className='text-blue-800'>
+              /dynamic
+            </Link>
+          </li>
+          <li>
+            <Link href='/realtime' className='text-blue-800'>
+              /realtime
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className='max-w-4xl mx-auto my-6 p-6 rounded-md bg-slate-200'></div>
+      <div className='max-w-4xl mx-auto my-6 p-6 rounded-md bg-slate-200'></div>
     </main>
   );
 }
